41bdb10c0a0500281cea8786e8228a17c5139674
AbeHaruhiko <caliconography@gmail.com>
2013-12-26 15:13:58 +0900

    メイン画面のボタンを廃止

--- SuicaPasmoReader/src/main/java/jp/caliconography/suicapasmoreader/MainActivity.java
+++ SuicaPasmoReader/src/main/java/jp/caliconography/suicapasmoreader/MainActivity.java
@@ -4,32 +4,28 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.os.AsyncTask;
-import android.os.Parcelable;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Button;
 import android.widget.TextView;
 
 import net.kazzz.AbstractNfcTagFragment;
 import net.kazzz.felica.FeliCaException;
 import net.kazzz.felica.NfcFeliCaTagFragment;
 import net.kazzz.felica.lib.FeliCaLib;
-import net.kazzz.felica.suica.Suica;
 
 import java.util.ArrayList;
 
 import jp.caliconography.suicapasmoreader.suica.HistoryBean;
 
-public class MainActivity extends ActionBarActivity  implements View.OnClickListener, AbstractNfcTagFragment.INfcTagListener {
+public class MainActivity extends ActionBarActivity  implements AbstractNfcTagFragment.INfcTagListener {
 
     private String TAG = this.getClass().getSimpleName();
     private AbstractNfcTagFragment mLastFragment;
@@ -93,82 +89,15 @@ public class MainActivity extends ActionBarActivity  implements View.OnClickList
                 break;
 
             case R.id.action_refresh:
-
-
-
-                try {
-                    final ProgressDialog dialog = new ProgressDialog(this);
-                    dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                    dialog.setIndeterminate(true);
-
-                    AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
-                        @Override
-                        protected void onPreExecute() {
-                            dialog.setMessage("読み込み中...");
-                            dialog.show();
-                        }
-
-                        @Override
-                        protected String doInBackground(Void... arg0) {
-                            try {
-                                if ( mLastFragment != null && mLastFragment instanceof NfcFeliCaTagFragment) {
-                                    NfcFeliCaTagFragment nfcf = (NfcFeliCaTagFragment)mLastFragment;
-                                    mHistories = nfcf.getFeliCaHistoryData();
-                                    return nfcf.dumpFeliCaHistoryData();
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            return "";
-                        }
-
-                        /* (non-Javadoc)
-                         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
-                         */
-                        @Override
-                        protected void onPostExecute(String result) {
-                            dialog.dismiss();
-                            TextView tv_tag = (TextView) findViewById(R.id.result_tv);
-                            if (result != null && result.length() > 0) tv_tag.setText(result);
-                        }
-                    };
-
-                    task.execute();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-
-
-
+                readHistory();
         }
 
 
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-
-
-
-    public void onClick(final View v) {
+    private void readHistory() {
         try {
-            final int id = v.getId();
-
             final ProgressDialog dialog = new ProgressDialog(this);
             dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
             dialog.setIndeterminate(true);
@@ -176,56 +105,20 @@ public class MainActivity extends ActionBarActivity  implements View.OnClickList
             AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
                 @Override
                 protected void onPreExecute() {
-                    switch (id) {
-                        case R.id.btn_read:
-                            dialog.setMessage("読み込み処理を実行中です...");
-                            break;
-                        case R.id.btn_write:
-                            dialog.setMessage("書き込み画面に移動中です...");
-                            break;
-                        case R.id.btn_hitory:
-                            dialog.setMessage("使用履歴を読み込み中です...");
-                            break;
-                    }
+                    dialog.setMessage(getString(R.string.MES_READING_HISTORY));
                     dialog.show();
                 }
 
                 @Override
                 protected String doInBackground(Void... arg0) {
-                    switch (id) {
-                        case R.id.btn_read:
-                            try {
-                                return mLastFragment.dumpTagData();
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            break;
-                        case R.id.btn_write:
-                            try {
-                                Intent intent =
-                                        new Intent(getApplicationContext(), DriveHelper.class);
-//                                intent.putExtra("nfcTag", mLastFragment.getNfcTag());
-                                intent.putExtra("histories", mHistories);
-                                startActivity(intent);
-                                return "";
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            break;
-                        case R.id.btn_hitory:
-                            try {
-                                if ( mLastFragment != null && mLastFragment instanceof NfcFeliCaTagFragment) {
-                                    NfcFeliCaTagFragment nfcf = (NfcFeliCaTagFragment)mLastFragment;
-//                                    return nfcf.dumpFeliCaHistoryData();
-                                    mHistories = nfcf.getFeliCaHistoryData();
-                                    return nfcf.dumpFeliCaHistoryData();
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            break;
-                        default:
-                            break;
+                    try {
+                        if ( mLastFragment != null && mLastFragment instanceof NfcFeliCaTagFragment) {
+                            NfcFeliCaTagFragment nfcf = (NfcFeliCaTagFragment)mLastFragment;
+                            mHistories = nfcf.getFeliCaHistoryData();
+                            return nfcf.dumpFeliCaHistoryData();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                     return "";
                 }
@@ -247,6 +140,23 @@ public class MainActivity extends ActionBarActivity  implements View.OnClickList
         }
     }
 
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+
 
     /* (non-Javadoc)
      * @see android.app.Activity#onNewIntent(android.content.Intent)
@@ -263,25 +173,6 @@ public class MainActivity extends ActionBarActivity  implements View.OnClickList
      */
     @Override
     public void onTagDiscovered(Intent intent, Parcelable nfcTag, AbstractNfcTagFragment fragment) {
-        //TextView tv_tag = (TextView) findViewById(R.id.result_tv);
-
-//        Button btnRead = (Button) findViewById(R.id.btn_read);
-//        btnRead.setOnClickListener(this);
-//
-        Button btnHistory = (Button) findViewById(R.id.btn_hitory);
-        if (btnHistory == null) {
-            btnHistory = (Button) getLayoutInflater().inflate(R.layout.fragment_main, null).findViewById(R.id.btn_hitory);
-        }
-//        btnHistory.setOnClickListener(this);
-//        btnHistory.setEnabled(false);
-//
-//        Button btnWrite = (Button) findViewById(R.id.btn_write);
-//        btnWrite.setOnClickListener(this);
-//        btnWrite.setEnabled(false);
-//
-//        Button btnInout = (Button) findViewById(R.id.btn_inout);
-//        btnInout.setOnClickListener(this);
-//        btnInout.setEnabled(false);
 
         try {
 
@@ -296,25 +187,16 @@ public class MainActivity extends ActionBarActivity  implements View.OnClickList
                             new FeliCaLib.IDm(intent.getByteArrayExtra(NfcAdapter.EXTRA_ID));
 
                     if ( idm == null ) {
-                        throw new FeliCaException("Felica IDm を取得できませんでした");
+                        throw new FeliCaException(getString(R.string.MES_FAILED_TO_GET_FELICA_ID));
                     }
 
-//                    btnHistory.setEnabled(!isFeliCatLite);
-//                    btnWrite.setEnabled(isFeliCatLite);
-
-//                    btnRead.performClick();
-                    btnHistory.performClick();
+                    readHistory();
 
                 } catch (Exception e) {
                     e.printStackTrace();
                     Log.e(TAG, e.toString());
                 }
-//                btnHistory.setEnabled(!isFeliCatLite);
-//                btnWrite.setEnabled(isFeliCatLite);
             } else {
-//                btnRead.performClick();
-//                btnHistory.setEnabled(false);
-//                btnWrite.setEnabled(true);
             }
 
         } catch ( Exception e ) {
